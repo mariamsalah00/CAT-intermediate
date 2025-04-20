3d67bfacf4cb2f40903f2794ce3295fde828f316
Mariam Salah <mariamsalah358@gmail.com>
2025-04-21 01:59:28 +0200

    Add files via upload

---
+++ js/18to22/script/main.js
@@ -0,0 +1,44 @@
+// assignment 1
+// Replace ? With Arithmetic Operators
+console.log(10 * 20 + 15 % 3 - 190 - 10 % 400); // 0
+// assignment 2 
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num  *(true + true)); // 6
+
+// Soultion Three
+console.log(++num + 2); // 6
+
+// Soultion Four
+console.log(--num + 3); // 6
+
+// Solution Five
+console.log((num += 2) + 1); // 6
+
+// Solution Six
+console.log(num + true); // 6
+ 
+// assignment 3
+let num2 = "10";
+
+// Solution One
+console.log(+num2 + +num2); // 20
+
+// Solution Two
+console.log(+num2 * (true + true)); // 20
+
+// Solution Three
+console.log(++num2 + 9); // 20
+
+// assignment 4
+let points = 10;
+++points;
+++points;
+++points;
+console.log(points); // 13
+points -= 5;
+console.log(points); // 8;
