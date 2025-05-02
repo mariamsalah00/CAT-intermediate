dd70c261f496dedaaf59749759fe9ddac76ac402
Mariam Salah <mariamsalah358@gmail.com>
2025-05-03 01:15:59 +0300

    Add files via upload

---
+++ week1/1to9/script/main.js
@@ -0,0 +1,29 @@
+// assignment 2
+console.log("%cMaiam", "color: blue;font-size: 80px;font-weight: bold;text-align: center;font-family: Arial");
+// assignment 3
+console.log("%cElzero %cWeb %cSchool", "color:red", "font-weight: bold ;color: green", "color: white;background-color: blue");
+// assignment 4   
+console.group("Group 1");
+console.log("Maiam");
+console.log("Maiam");
+console.group("Child Group");
+console.log("Maiam");
+console.log("Maiam");
+console.group("Grand Child Group");
+console.log("Maiam");
+console.log("Maiam");
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("Group 2");
+console.log("Maiam");
+console.log("Maiam");
+console.groupEnd();
+// assignment 5
+console.table(["mariam", "salah", "mohamed"]);
+// assignment 6
+/* console.log("Iam In Console");
+document.write("Iam In Page");*/
+
+
+
